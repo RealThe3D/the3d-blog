fec1b51e340bffdabe5c53fb4388eaddbc7c48ff
The3D <3dninja54@gmail.com>
2023-10-15 21:36:34 -0400

    🐛Forgot to import css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+import "node_modules/highlight.js/styles/github-dark.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
