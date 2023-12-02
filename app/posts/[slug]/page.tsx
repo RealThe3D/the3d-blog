3f46869896f82a82ca841dbb7d77fa837e1bb9ea
The3D <3dninja54@gmail.com>
2023-12-02 12:43:13 -0500

    ✏️fixed missing semicolon

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { addMinutes, format } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import Image from "next/image";
-import { useMDXComponent } from "next-contentlayer/hooks";
+import { getMDXComponent } from "next-contentlayer/hooks";
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
@@ -18,7 +18,7 @@ const PostPage = ({ params }: { params: { slug: string } }) => {
     addMinutes(new Date(post.date), new Date(post.date).getTimezoneOffset()),
     "MMMM d, yyyy"
   );
-  const MDXContent = useMDXComponent(post.body.code);
+  const MDXContent = getMDXComponent(post.body.code);
 
   return (
     <div className="flex flex-col justify-center divide-y mx-auto py-12 max-w-5xl">
@@ -36,8 +36,8 @@ const PostPage = ({ params }: { params: { slug: string } }) => {
       <section className="flex flex-col md:flex-row py-4">
         <div className="flex flex-col w-2/3 md:w-1/4 font-medium divide-y place-self-center md:place-self-start">
           <div className="text-xs text-gray-500 dark:text-gray-400 pt-2 pb-4">
-            <h2>Tags</h2>
-            <p className="text-sm">{post.category}</p>
+            <h2>Tags:</h2>
+            <p className="text-sm">{post.categories.join(", ")}</p>
           </div>
           <div className="text-xs text-gray-500 dark:text-gray-400 py-6">
             <h2>Read Time:</h2>
@@ -45,7 +45,9 @@ const PostPage = ({ params }: { params: { slug: string } }) => {
           </div>
           <div className="text-xs text-gray-500 dark:text-gray-400 py-6">
             <h2>Date Published:</h2>
-            <p className="text-sm">{postDate}</p>
+            <time className="text-sm" dateTime={postDate}>
+              {postDate}
+            </time>
           </div>
         </div>
         <article className="prose p-4 dark:prose-invert dark:prose-hotpink">
