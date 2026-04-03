fc453bce19b6f551d394923d5698de486231feec
The3D <3dninja54@gmail.com>
2026-04-03 15:17:57 -0400

    ✨ create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import Divider from "@/components/Divider";
+
+const Footer = () => {
+  return (
+    <>
+      <Divider />
+      <div className="text-center py-7 px-10 text-stone-450 text-sm">
+        <span>Made with 💖 from Florida. Open Source on GitHub</span>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
