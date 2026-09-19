403d79b22cbe08098607ba569b9fa60d436204ce
The3D <3dninja54@gmail.com>
2026-09-19 00:39:06 -0400

    improve: opengraph metadata on root and post pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,20 +5,45 @@ import Hero from "@/components/Hero";
 import { getAllPosts } from "@/lib/postLoader";
 
 export const Route = createFileRoute("/")({
-  loader: async () => {
-    return getAllPosts();
-  },
-  component: Home,
+	loader: async () => {
+		return getAllPosts();
+	},
+	head: () => ({
+		meta: [
+			{ title: "The3D" },
+			{ property: "og:title", content: "The3D's Blog" },
+			{ property: "og:image", content: "/proto_mirage_pfp.jpg" },
+			{
+				property: "og:description",
+				content: "A blog site made by The3D.",
+			},
+			{
+				property: "og:url",
+				content: `https://the3d.vercel.app`,
+			},
+			{ name: "twitter:title", content: "The3D's Blog" },
+			{
+				name: "twitter:description",
+				content: "A blog site made by The3D",
+			},
+			{ name: "twitter:image", content: "/proto_mirage_pfp.jpg" },
+			{
+				name: "twitter:url",
+				content: `https://the3d.vercel.app/`,
+			},
+		],
+	}),
+	component: Home,
 });
 
 function Home() {
-  const posts = Route.useLoaderData();
+	const posts = Route.useLoaderData();
 
-  return (
-    <>
-      <Hero />
-      <AllBlogs posts={posts} />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Hero />
+			<AllBlogs posts={posts} />
+			<Footer />
+		</>
+	);
 }
--- src/routes/posts/$slug.tsx
+++ src/routes/posts/$slug.tsx
@@ -7,7 +7,32 @@ import { loadPost } from "@/lib/postLoader";
 export const Route = createFileRoute("/posts/$slug")({
 	loader: ({ params }) => loadPost({ data: { title: params.slug } }),
 	component: RouteComponent,
-	// TODO: Add post-specific metadata
+	head: ({ loaderData, params }) => ({
+		meta: [
+			{ title: loaderData?.frontmatter.title },
+			{ name: "description", content: loaderData?.frontmatter.description },
+			{ property: "og:title", content: loaderData?.frontmatter.title },
+			{
+				property: "og:description",
+				content: loaderData?.frontmatter.description,
+			},
+			{ property: "og:image", content: loaderData?.frontmatter.cover },
+			{
+				property: "og:url",
+				content: `https://the3d.vercel.app/posts/${params.slug}`,
+			},
+			{ name: "twitter:title", content: loaderData?.frontmatter.title },
+			{
+				name: "twitter:description",
+				content: loaderData?.frontmatter.description,
+			},
+			{ name: "twitter:image", content: loaderData?.frontmatter.cover },
+			{
+				name: "twitter:url",
+				content: `https://the3d.vercel.app/posts/${params.slug}`,
+			},
+		],
+	}),
 });
 
 function RouteComponent() {
@@ -52,87 +77,3 @@ function RouteComponent() {
 		</>
 	);
 }
-// import { addMinutes, format } from "date-fns";
-// import Image from "next/image";
-// import { notFound } from "next/navigation";
-// import { posts } from "@/.velite";
-// import BackToPosts from "@/components/BackToPosts";
-// import { MDXContent } from "@/components/Mdx";
-// import ProgressBar from "@/components/ProgressBar";
-
-// const getPostBySlug = async (slug: string) => {
-//   return posts.find((post) => post.slug === slug);
-// };
-
-// const PostPage = async ({ params }: PostProps) => {
-//   const { slug } = await params;
-//   const post = getPostBySlug(slug);
-
-//   if (!post) return notFound();
-
-//   const postDate = format(
-//     addMinutes(new Date(post.date), new Date(post.date).getTimezoneOffset()),
-//     "MMMM d, yyyy",
-//   );
-
-//   return (
-//     <>
-//       <ProgressBar />
-//       <div className="prose dark:prose-stone prose-img:rounded-lg dark:prose-invert mx-auto p-6">
-//         <BackToPosts />
-//         <header className="flex flex-col">
-//           <div className="flex gap-2.5 items-center text-sm py-6">
-//             <span className="uppercase bg-secondary dark:bg-blue-950 text-blue-950 dark:text-secondary px-3 rounded-2xl text-xs py-1 tracking-wider">
-//               {post.categories[0]}
-//             </span>
-//             <span className="border-stone-450 dark:border-white/15 border-2 rounded-full" />
-//             <span className="text-stone-450">{postDate}</span>
-//             <span className="dark:border-white/15 border-2 rounded-full" />
-//             <span className="text-stone-450">{post.readTime} min read</span>
-//           </div>
-//           <h1>{post.title}</h1>
-//           <Image
-//             className="h-80 object-cover border rounded-xl mt-0"
-//             width={1080}
-//             height={1080}
-//             src={post.cover}
-//             alt="ai generated cover image"
-//           />
-//         </header>
-//         <article>
-//           <MDXContent code={post.content} />
-//         </article>
-//       </div>
-//     </>
-//   );
-// };
-
-// export async function generateMetadata({ params }: PostProps) {
-//   const { slug } = await params;
-//   const post = getPostBySlug(slug);
-//   if (post == null) return {};
-
-//   const coverUrl = post.cover;
-
-//   return {
-//     title: post.title,
-//     description: post.description,
-//     openGraph: {
-//       title: post.title,
-//       description: post.description,
-//       images: coverUrl,
-//     },
-//     twitter: {
-//       card: "summary_large_image",
-//       title: post.title,
-//       description: post.description,
-//       images: coverUrl,
-//     },
-//   };
-// }
-
-// export function generateStaticParams() {
-//   return posts.map((post) => ({ slug: post.slug }));
-// }
-
-// export default PostPage;
